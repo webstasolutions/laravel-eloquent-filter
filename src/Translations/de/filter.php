b5fa3b5bdb476537c38c3e591af5a7a01730e918
Lukas Vana <fabian@fabian.cz>
2018-05-21 16:53:39 +0200

    DE translations

---
+++ src/Translations/de/filter.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'from' => 'von',
+    'to' => 'bis',
+
+    'filter' => 'Filter',
+    'reset' => 'zurücksetzen',
+    'clear' => 'zurücksetzen',
+
+    'per_page' => 'pro Seite'
+];
