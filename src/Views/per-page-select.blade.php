d79432c651ddfd46cd4fce044ce796468c4ca32d
Lukas Verner <lukasverner@icloud.com>
2023-10-03 10:34:52 +0200

    Added script to keep filter choices after changing per page value

--- src/Views/per-page-select.blade.php
+++ src/Views/per-page-select.blade.php
@@ -5,4 +5,26 @@
             <option value="{{ $val }}" @if($val == $value) selected @endif>{{ $val }}</option>
         @endforeach
     </select>
+    <script>
+        (function () {
+            var select = document.querySelector('select[name="{{ $prefix }}_per_page"]');
+            if(select.closest("form") === null || select.closest("form").id != 'table-filter-controls-{{ $prefix }}') {
+                var input = document.createElement("input");
+                input.type = "text";
+                input.name = "{{ $prefix }}_per_page";
+                input.hidden = true;
+                input.value = '{{ $value }}';
+                document.querySelector('#table-filter-controls-{{ $prefix }}').appendChild(input);
+                select.addEventListener('change', function () {
+                    input.value = this.value;
+                    document.getElementById('{{ $prefix }}_submit_filter').click();
+                    console.log(this.value);
+                });
+            }else {
+                select.addEventListener('change', function () {
+                    document.getElementById('{{ $prefix }}_submit_filter').click();
+                });
+            }
+        })();
+    </script>
 </div>
